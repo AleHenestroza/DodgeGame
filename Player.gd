3a180fe27aa09bf2084fb4e32e5c0b69fd12dbe8
Alejandro Henestroza <alehenestroza@gmail.com>
2022-07-06 20:24:10 -0300

    Applied correct animation to each move direction

--- Player.gd
+++ Player.gd
@@ -5,10 +5,12 @@ var screen_size # Size of the game window.
 
 func _ready():
 	screen_size = get_viewport_rect().size
+	hide()
 	
 func _process(delta):
 	var velocity = Vector2.ZERO # The player's movement vector.
 	
+	# Check input
 	if Input.is_action_pressed("move_right"):
 		velocity.x += 1
 	if Input.is_action_pressed("move_left"):
@@ -18,12 +20,24 @@ func _process(delta):
 	if Input.is_action_pressed("move_down"):
 		velocity.y += 1
 	
+	# Play animation if player is moving (normalize velocity vector (so that moving in two directions
+	# won't result in a higher speed than moving in just one
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
 		$AnimatedSprite.play()
 	else:
 		$AnimatedSprite.stop()
 	
+	# Clamp the player to the window
 	position += velocity * delta
 	position.x = clamp(position.x, 0, screen_size.x)
 	position.y = clamp(position.y, 0, screen_size.y)
+	
+	# Define which animation should be used
+	if velocity.x != 0:
+		$AnimatedSprite.animation = "walk"
+		$AnimatedSprite.flip_v = false
+		$AnimatedSprite.flip_h = velocity.x < 0
+	if velocity.y != 0:
+		$AnimatedSprite.animation = "up"
+		$AnimatedSprite.flip_v = velocity.y > 0
